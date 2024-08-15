4ccadd51740c19f4da3033d970760e979cb0537d
Martin Lievano <109536925+martinlievano@users.noreply.github.com>
2024-08-14 22:13:57 -0500

    04_data_analysis.sql

---
+++ 04_data_analysis.sql
@@ -0,0 +1,7 @@
+SELECT * INTO OUTFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\cyclistic_cleaned' 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n' 
+FROM nombre_de_tu_tabla;
+
+
